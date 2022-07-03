63b848b9a1c7954c68251675673a6b78337ca911
MohannadHamdy <mohannad7amdy@gmail.com>
2022-07-03 15:37:50 +0200

    fixed useFetch

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,46 +1,32 @@
 import { useState, useEffect } from "react";
 
-export const useFetch = ({ url }) => {
+export const useFetch = (url) => {
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
 
   useEffect(() => {
-    const controller = new AbortController();
+    setLoading(true);
 
     const fetchData = async () => {
-      setLoading(true);
       try {
-        const response = await fetch(url, { signal: controller.signal });
-        // if (response && response.ok) {
-        //   const json = await response.json();
-        //   setData(json);
-        //   setLoading(false);
-        //   setError(null);
-        // } else {
-        //   throw new Error(response.statusText);
-        // }
-        if (!response.ok) {
+        const response = await fetch(url);
+        if (response && response.ok) {
+          const json = await response.json();
+          setData(json);
+          setLoading(false);
+          setError(null);
+        } else {
           throw new Error(response.statusText);
         }
-        console.log("response from usefetch", response);
-        const json = await response.json();
-        setLoading(false);
-        console.log("data from usefetch", json);
-        setData(json);
-        setError(null);
       } catch (error) {
-        if (error.name === "AbortError") {
-          console.log("the fetch was aborted");
-        } else {
-          setError(error.message);
-          setLoading(false);
-        }
+        setError(error.message);
+        setLoading(false);
       }
     };
 
     fetchData();
-  }, [url, data]);
+  }, [url]);
 
   return { data, loading, error };
 };
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,42 +1,16 @@
 import "./home.scss";
-import { useFetch } from "../../hooks/useFetch";
-import { useState, useEffect } from "react";
 import RecipeList from "../../components/RecipeList";
+import { useFetch } from "../../hooks/useFetch";
 
 const Home = () => {
-  // const { error, loading, data } = useFetch(
-  //   "https://jsonplaceholder.typicode.com/todos/"
-  // );
-  const [data, setData] = useState(null);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
+  const { error, loading, data } = useFetch(`http://localhost:3000/recipes`);
 
-  useEffect(() => {
-    const controller = new AbortController();
+  // useEffect(() => {
+  //   axios.get("https://jsonplaceholder.typicode.com/todos/").then((res) => {
+  //     console.log(res.data);
+  //   });
+  // }, []);
 
-    const fetchData = async () => {
-      try {
-        const response = await fetch("http://localhost:3000/recipes", {
-          signal: controller.signal,
-        });
-        if (!response.ok) {
-          throw new Error(response.statusText);
-        }
-        const json = await response.json();
-
-        setData(json);
-      } catch (error) {
-        if (error.name === "AbortError") {
-          console.log("the fetch was aborted");
-        } else {
-          setError(error.message);
-          setLoading(false);
-        }
-      }
-    };
-
-    fetchData();
-  }, []);
   return (
     <div className="home">
       {error && <p className="error">Error: {error}</p>}
